df8310ba069dc0a47b7b5d9426fd88e7d21986c5
jcjohncarter3 <jcjohncarter3@gmail.com>
2024-11-10 18:09:52 -0600

    updater server.js

--- server/server.js
+++ server/server.js
@@ -1,11 +1,10 @@
 const { ApolloServer } = require("apollo-server-express");
-const mongoose = require("mongoose");
 const express = require("express");
 const path = require("path");
 const db = require("./config/connection");
 const routes = require("./routes");
 const { typeDefs, resolvers } = require("./schemas"); // Adjust the path as necessary
-const authMiddleware = require("./utils/auth");
+const { authMiddleware } = require("./utils/auth"); //
 
 const app = express();
 const PORT = process.env.PORT || 3001;
